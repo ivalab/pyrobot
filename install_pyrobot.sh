a73892a28a8f29b46887224f16f7461bc83e578e
jpeng303 <jpeng303>
2024-11-07 10:36:39 -0800

    More jank to make it build in ubuntu 20

--- install_pyrobot.sh
+++ install_pyrobot.sh
@@ -56,19 +56,27 @@ fi
 echo "Python $PYTHON_VERSION chosen for pyRobot installation."
 
 sudo_installs=()
-sudo_installs+=(python-virtualenv software-properties-common)
-sudo_installs+=(ros-$ROS_NAME-orocos-kdl ros-$ROS_NAME-kdl-parser-py ros-$ROS_NAME-python-orocos-kdl ros-$ROS_NAME-trac-ik ros-$ROS_NAME-cv-bridge)
+if [ $ubuntu_version == "20.04" ]; then
+    sudo_installs+=(ros-$ROS_NAME-kdl-parser-py ros-$ROS_NAME-trac-ik ros-$ROS_NAME-cv-bridge)
+else
+    sudo_installs+=(ros-$ROS_NAME-orocos-kdl ros-$ROS_NAME-kdl-parser-py ros-$ROS_NAME-python-orocos-kdl ros-$ROS_NAME-trac-ik ros-$ROS_NAME-cv-bridge)
+fi
 
 if [ $PYTHON_VERSION == "3" ]; then
-    sudo_installs+=(python-catkin-tools python3-dev python3-catkin-pkg-modules python3-numpy python3-yaml)
     sudo_installs+=(python3-tk python-is-python3)
     if [ $ubuntu_version == "16.04" ]; then
+        sudo_installs+=(python-virtualenv software-properties-common)
+        sudo_installs+=(python-catkin-tools python3-dev python3-catkin-pkg-modules python3-numpy python3-yaml)
         sudo_installs+=(python-software-properties)
         sudo add-apt-repository -y ppa:fkrull/deadsnakes
         #sudo add-apt-repository ppa:jonathonf/python-3.6
     elif [ $ubuntu_version == "20.04" ]; then
+        sudo_installs+=(python3-venv)
+        sudo_installs+=(python3-catkin-tools python3-dev python3-catkin-pkg-modules python3-numpy python3-yaml)
         sudo_installs+=(python3.8-venv)
     fi
+else
+    sudo_installs+=(python-virtualenv software-properties-common)
 fi
 
 if (sudo -v) then
@@ -77,7 +85,7 @@ if (sudo -v) then
 else
     echo "sudo apt-get update" > sudo_install.sh
     echo "sudo apt-get install ${sudo_installs[*]}" >> sudo_install.sh
-    echo "User cannot run sudo. Please run: 'sudo sudo_install.sh' on a user that can."
+    echo "User cannot run sudo. Please run: 'sudo sh sudo_install.sh' on a user that can."
     read -p "Press enter to continue:" _
 fi
 
@@ -126,22 +134,27 @@ if [ $PYTHON_VERSION == "3" ]; then
             git clone -b indigo-devel https://github.com/ros/geometry2
             git clone -b python3_patch https://github.com/kalyanvasudev/vision_opencv.git
             git clone -b patch-1 https://github.com/kalyanvasudev/ros_comm.git
+            BUILD_CMD=catkin_make
         elif [ $ROS_NAME == "melodic" ]; then
             git clone -b melodic-devel https://github.com/ros/geometry
             git clone -b melodic-devel https://github.com/ros/geometry2
             git clone -b python3_patch_melodic https://github.com/kalyanvasudev/vision_opencv.git
             git clone -b patch-1 https://github.com/kalyanvasudev/ros_comm.git
+            BUILD_CMD=catkin_make
         elif [ $ROS_NAME == "noetic" ]; then
             git clone -b noetic-devel https://github.com/ros/geometry
             git clone -b noetic-devel https://github.com/ros/geometry2
             git clone -b noetic https://github.com/ros-perception/vision_opencv.git
             git clone -b noetic-devel https://github.com/ros/ros_comm.git
+            BUILD_CMD="catkin build"
         fi
 
         cd ..
 
+        . /opt/ros/$ROS_NAME/setup.bash
+
         # Build
-        catkin_make --cmake-args -DPYTHON_EXECUTABLE=$(which python) -DPYTHON_INCLUDE_DIR=$(ls usr/include/ | grep python3 | head -n1) -DPYTHON_LIBRARY=$(ls /usr/lib/x86_64-linux-gnu/ | grep libpython3.*\.so\$ | head -n1)
+        $BUILD_CMD --cmake-args -DPYTHON_EXECUTABLE=$(which python) -DPYTHON_INCLUDE_DIR=$(ls usr/include/ | grep python3 | head -n1) -DPYTHON_LIBRARY=$(ls /usr/lib/x86_64-linux-gnu/ | grep libpython3.*\.so\$ | head -n1)
         
         echo "alias load_pyrobot_env='source $VIRTUALENV_FOLDER/bin/activate && source $PYROBOT_PYTHON3_WS/devel/setup.bash'" >> ~/.bashrc
     fi
